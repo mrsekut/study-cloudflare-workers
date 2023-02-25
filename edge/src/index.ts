c2050f5f00aab5e3512a65a81e9908851d79eed7
mrsekut <k.cloudspider@gmail.com>
2023-02-25 22:38:11 +0900

    8787にアクセスすれば3000を見るようにした

--- edge/src/index.ts
+++ edge/src/index.ts
@@ -2,16 +2,10 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-	return c.text("Hello World!");
-});
-
-// app.get("/ping", (c) => {
-// 	return c.text("pong");
-// });
-
-app.get("/json", (c) => {
-	return c.json({ hoge: "fuga" });
+app.all("*", (c) => {
+	const url = new URL(c.req.url);
+	url.port = "3000"; // ここで、workersに来たものを、Next.jsに渡している. 8787 -> 3000
+	return fetch(url, { headers: c.req.headers, body: c.req.body });
 });
 
 export default app;
