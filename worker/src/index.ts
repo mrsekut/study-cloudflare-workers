a5ac6b4f6f6030a0cb281b6e82dfa55a41e39da0
mrsekut <k.cloudspider@gmail.com>
2023-04-22 17:28:45 +0900

    feat: ISRの再実装

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -2,6 +2,41 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
+// ISRを再実装したもの
+app.get("/isr_todos", async (c) => {
+	const request = new Request(
+		"https://todo-nextjs-b5lmrttwpa-an.a.run.app/todos",
+		{ headers: c.req.headers }
+	);
+
+	const cache = caches.default;
+	let response = await cache.match(request);
+
+	if (response) {
+		const expires = response.headers.get("expires");
+
+		// expiresが切れている場合はcacheを更新する.
+		if (expires && new Date(expires) < new Date()) {
+			createCache(c.executionCtx.waitUntil, request);
+		}
+
+		return response; // 1個古いやつを返している
+	}
+
+	// cacheがない場合
+	response = await fetch(request);
+	response = new Response(response.body, response);
+	response.headers.set("Cache-Control", "max-age=300000"); // とても長いTTLを設定しておく
+
+	const expiresDate = new Date();
+	expiresDate.setMinutes(expiresDate.getMinutes() + 1); // 1分後にcacheを更新する.
+	response.headers.set("expires", expiresDate.toUTCString());
+
+	c.executionCtx.waitUntil(cache.put(request, response.clone()));
+
+	return new Response("test");
+});
+
 app.get("/todos", async (c) => {
 	const request = new Request(
 		"https://todo-nextjs-b5lmrttwpa-an.a.run.app/todos",
@@ -33,3 +68,22 @@ app.all("*", (c) => {
 });
 
 export default app;
+
+const createCache = async (
+	waitUntil: (promise: Promise<unknown>) => void,
+	request: Request
+) => {
+	const cache = caches.default;
+	let response = await fetch(request);
+
+	// cacheの保存期間を指定する
+	response = new Response(response.body, response);
+	response.headers.set("Cache-Control", "max-age=3000000"); // とても長いTTLを設定しておく
+
+	// cacheの有効期限を指定する
+	const expiresDate = new Date();
+	expiresDate.setMinutes(expiresDate.getMinutes() + 1); // 1分後にcacheを更新する.
+	response.headers.set("expires", expiresDate.toUTCString());
+
+	waitUntil(cache.put(request, response.clone()));
+};
