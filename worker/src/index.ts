d9547635f241bca85fc3747cb589576739229f3e
mrsekut <k.cloudspider@gmail.com>
2023-04-22 16:57:08 +0900

    feat: proxyでcacheする

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -2,12 +2,34 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/todos", (c) => {
-	return fetch("https://todo-nextjs-b5lmrttwpa-an.a.run.app/todos");
+app.get("/todos", async (c) => {
+	const request = new Request(
+		"https://todo-nextjs-b5lmrttwpa-an.a.run.app/todos",
+		{ headers: c.req.headers }
+	);
+
+	const cache = caches.default; // workerのやつ.
+	let response = await cache.match(request); // reqにcacheがあるかどうかを確認し、あれば取り出す
+
+	if (response) {
+		return response;
+	}
+
+	// cacheがない場合
+	response = await fetch(request);
+	response = new Response(response.body, response); // responseをcloneする. (headerを書き換えるため)
+	response.headers.set("Cache-Control", "max-age=30"); // 30秒間cacheする.
+	c.executionCtx.waitUntil(cache.put(request, response.clone())); // 非同期でcacheにresponseを保存する. (resを返しつつ、非同期で保存)
+
+	return response;
 });
 
 app.all("*", (c) => {
-	return c.text("aa");
+	const url = new URL(c.req.url);
+	const requestUrl = new URL("https://todo-nextjs-b5lmrttwpa-an.a.run.app");
+	requestUrl.pathname = url.pathname;
+
+	return fetch(requestUrl, { headers: c.req.headers });
 });
 
 export default app;
